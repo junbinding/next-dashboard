e9ed2f36d8cd79cac2a6fbc56377b88ee9ca665e
JunbinDing <junbinding@gmail.com>
2025-05-06 10:40:40 +0800

    polish: 连接数据库

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -1,3 +1,10 @@
+import Image from "next/image";
+
 export default function Page() {
-    return <p>Invoices Page</p>
+    return (
+        <div>
+            <p>Invoices Page</p>
+            <Image src="/hero-mobile.png" alt="Invoices" width={500} height={500} />
+        </div>
+    )
 }
